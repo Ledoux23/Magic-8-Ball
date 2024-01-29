e719b209e08143766fbc42e82b2039ed75cda6b3
TESSOH TENE <michel.tessohtene@cgi.com>
2024-01-29 12:20:07 +0100

    Vérification de la méthode askQuestion

--- src/main/java/com/project/magic8ball/controller/Magic8BallController.java
+++ src/main/java/com/project/magic8ball/controller/Magic8BallController.java
@@ -22,12 +22,33 @@ public class Magic8BallController {
         return "magic8ball";
     }
 
-    @PostMapping("/ask")
+   /* @PostMapping("/ask")
     public ResponseEntity<String> askQuestion(@RequestParam String question) {
         String response = generateResponse();
         responses.add(response);
         return ResponseEntity.ok(response);
+    }*/
+    
+    @PostMapping("/ask")
+    public ResponseEntity<String> askQuestion(@RequestParam String question) {
+        if (isValidQuestion(question)) {
+            String response = generateResponse();
+            responses.add(response);
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.badRequest().body("Invalid question");
+        }
     }
+
+    private boolean isValidQuestion(String question) {
+        // Vérifier si la question n'est pas vide et comporte au moins un mot
+        if (question != null && !question.trim().isEmpty()) {
+            // Vérifier si la question se termine par un point d'interrogation
+            return question.trim().endsWith("?");
+        }
+        return false;
+    }
+
 
     private String generateResponse() {
         // Liste des réponses possibles
